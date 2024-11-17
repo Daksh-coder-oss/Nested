118ec164d35fbb50793bb56496f96b913a7eade7
Daksh-coder-oss <vv0039190@gmail.com>
2024-11-17 10:33:16 +0000

    Add files via upload

---
+++ checking_age.py
@@ -0,0 +1,7 @@
+age=int(input("PLease enter your age"))
+attendance=int(input("Please enter your attendence"))
+if age<=20 and attendance>=75:
+    print("You can proceed on")
+else:
+    print("Go back home")    
+    
